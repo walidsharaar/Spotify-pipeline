0a3492e67e8d31725d0fb8642613a5bb2a3f9b34
walid <walidsharar@gmail.com>
2022-06-25 23:21:01 +0200

    working on youtube client file

---
+++ youtube_client.py
@@ -0,0 +1,12 @@
+class YouTubeClient(object):
+    def __init__(self):
+        pass
+
+    def get_playlists(self):
+        pass
+
+    def get_videos_from_playlist(self, playlist_id):
+        pass
+
+    def get_artist_and_track_from_video(self):
+        pass
